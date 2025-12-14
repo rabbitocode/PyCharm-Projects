0ec8fa5db0e7eda6e364f2072e9c7aa62529bfc0
Rabbit <davidbengtsson374@gmail.com>
2025-12-14 12:50:53 +0100

    Folder Rename

---
+++ Birthday Wisher/Birthday Wisher/main.py
@@ -0,0 +1,49 @@
+import smtplib
+import datetime as dt
+import random
+import pandas
+
+
+
+
+
+
+
+
+
+
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+
+if year == 2020:
+    print("Wear a face mask")
+
+
+
+
+data_of_birth = dt.datetime(year=2003, month=10, day=17)
+
+
+
+if day_of_week == 0:
+
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+
+    my_email = ""
+    password = ""
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="",
+            msg=f"Subject:Motivational Quote\n\n{quote}"
+        )
